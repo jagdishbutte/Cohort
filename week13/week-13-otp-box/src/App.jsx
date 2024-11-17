81b440e10ce99cb0b99e8bf8839ee5f31b28cf1a
jagdishbutte <jagdishbutte@gmail.com>
2024-11-17 16:10:14 +0530

    Added week 13

---
+++ week13/week-13-otp-box/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='h-screen bg-blue-700'>
+      <Sidebar1 /> 
+      <Sidebar4 />
+    </div>
+  )
+}
+
+export default App
